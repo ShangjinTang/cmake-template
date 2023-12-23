e8440d296a0c642df97dcbf44a28c5ad7d8aa42c
Shangjin Tang <shangjin.tang@gmail.com>
2023-12-23 12:41:48 +0800

    fix scripts compile_run_* missing CMAKE_BUILD_TYPE

--- compile_run_without_conan.sh
+++ compile_run_without_conan.sh
@@ -21,7 +21,7 @@ print_seperate_line "1. Cleaning"
 rm -rf build &> /dev/null
 
 print_seperate_line "2. CMake: preparing build"
-cmake -S . -B build/Debug -DUSE_CONAN=FALSE
+cmake -S . -B build/Debug -DCMAKE_BUILD_TYPE=DEBUG -DUSE_CONAN=FALSE
 cp build/Debug/compile_commands.json .
 
 print_seperate_line "3. CMake: building"
