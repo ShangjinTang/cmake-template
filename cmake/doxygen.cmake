9fe63542f48bdeac9c5e00452880ce03883b39e5
Shangjin Tang <shangjin.tang@gmail.com>
2023-12-31 21:35:13 +0800

    use doxygen option DOXYGEN_EXCLUDE_PATTERNS instead of manually variable DOC_DIRS

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -2,27 +2,23 @@
 #
 # doxygen documetation generate
 
-find_package(Doxygen COMPONENTS dot)
-if(Doxygen_FOUND)
-  message(STATUS "Found package: doxygen with component dot")
-  set(DOXYGEN_CALLER_GRAPH YES)
-  set(DOXYGEN_CALL_GRAPH YES)
-  set(DOXYGEN_EXTRACT_ALL YES)
-  set(DOXYGEN_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/docs)
+if(ENABLE_DOXYGEN)
+  find_package(Doxygen COMPONENTS dot)
+  if(Doxygen_FOUND)
+    message(STATUS "Found package: doxygen with component dot")
+    set(DOXYGEN_CALLER_GRAPH YES)
+    set(DOXYGEN_CALL_GRAPH YES)
+    set(DOXYGEN_EXTRACT_ALL YES)
+    set(DOXYGEN_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/docs)
+    set(DOXYGEN_EXCLUDE_PATTERNS "*/test/*" "*/external/*")
 
-  if(DEFINED DOC_DIRS AND NOT "${DOC_DIRS}" STREQUAL "")
-    foreach(doc_dir ${DOC_DIRS})
-      if(NOT IS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${doc_dir})
-        message(
-          SEND_ERROR
-            "Directory ${CMAKE_CURRENT_SOURCE_DIR}/${doc_dir} does not exist.")
-      endif()
-    endforeach()
-    doxygen_add_docs(doxygen-docs ALL ${CMAKE_CURRENT_SOURCE_DIR}/${DOC_DIRS})
+    doxygen_add_docs(doxygen-docs ALL ${CMAKE_CURRENT_SOURCE_DIR})
     message("doxygen with component dot finished setting up.")
-  endif()
 
-else()
-  message(WARNING "doxygen with component dot requested but not found.
+  else()
+
+    message(WARNING "doxygen with component dot requested but not found.
     Please install 'doxygen' and 'graphviz' first.")
+
+  endif()
 endif()
