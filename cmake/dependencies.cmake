883929674672832d7bc3add54c78a317bb71d5ae
Shangjin Tang <shangjin.tang@gmail.com>
2024-11-19 12:05:02 +0800

    fix(dependencies): fix gtest lib not found in CLion

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -7,8 +7,8 @@ find_package(spdlog CONFIG REQUIRED)
 find_package(Microsoft.GSL CONFIG REQUIRED)
 find_package(CLI11 CONFIG REQUIRED)
 if(BUILD_TESTING)
-  find_package(doctest CONFIG REQUIRED)
-  find_package(GTest CONFIG REQUIRED)
+    find_package(GTest CONFIG REQUIRED)
+    find_package(doctest CONFIG REQUIRED)
 endif()
 
 # ##############################################################################
@@ -20,7 +20,9 @@ list(APPEND COMMON_LINKED_LIBRARIES Microsoft.GSL::GSL)
 list(APPEND COMMON_LINKED_LIBRARIES CLI11::CLI11)
 
 # test libraries
-list(APPEND COMMON_LINKED_TEST_LIBRARIES gtest gtest_main)
-list(APPEND COMMON_LINKED_TEST_LIBRARIES doctest::doctest)
+if(BUILD_TESTING)
+    list(APPEND COMMON_LINKED_TEST_LIBRARIES GTest::gtest GTest::gtest_main)
+    list(APPEND COMMON_LINKED_TEST_LIBRARIES doctest::doctest)
+endif()
 
 # ##############################################################################
